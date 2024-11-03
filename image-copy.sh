a087c80879e71181574b162169587aacdc18933d
diejia <diejia@webuy.ai>
2024-11-03 22:19:39 +0800

    m

--- image-copy.sh
+++ image-copy.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
 IMAGE_ID=$1
+# 源镜像ID
+segs=$(echo "$IMAGE_ID" | awk -F'/' '{print NF}')
+if [ ! "$segs" == "3" ]; then
+	IMAGE_ID=docker.io/$IMAGE_ID
+fi
 # 构建目标镜像ID
 DEST_IMAGE_ID=""
 if [ -z "$REGISTRY_NAMESPACE" ]; then
 	DEST_IMAGE_ID=$REGISTRY_HOST/$IMAGE_ID
 else
-	DEST_IMAGE_ID=$REGISTRY_HOST/$REGISTRY_NAMESPACE/$(echo "$IMAGE_ID" | tr '/' '-')
+	DEST_IMAGE_ID=$REGISTRY_HOST/$REGISTRY_NAMESPACE/$(echo "$IMAGE_ID" | tr '/' '_')
 fi
 
 # login
